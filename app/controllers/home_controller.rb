2b27165b4b8fef696562cf4a2ed4cf393779fce3
krongk <kenrome@gmail.com>
2013-03-31 19:08:57 +0800

    add chaches

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 class HomeController < ApplicationController
   include ApplicationHelper
+  caches_page :index, :metro_icons
   
   def index
   	@pages = Page.all.sort{|a, b| [a.data_row, a.data_col] <=> [b.data_row, b.data_col] }.paginate(:per_page => 24, :page => params[:page] || 1)
